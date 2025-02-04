dac0fd788d828954de210c8563f04b509990d08a
AlexGibby2000 <alexandergibby2000@gmail.com>
2025-02-04 13:42:26 -0500

    Added OCR functionality using Tesseract

---
+++ src/ocr.py
@@ -0,0 +1,21 @@
+import pytesseract
+from pdf2image import convert_from_path
+import os
+import cv2
+
+TESSERACT_PATH = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+
+def extract_text_from_pdf(pdf_path):
+    images = convert_from_path(pdf_path)
+    text = ''
+    for img in images:
+        img_cv = cv2.cvtColor(cv2.imread(img.filename), cv2.COLOR_BGR2RGB)
+        text += pytesseract.image_to_string(img_cv)+'\n'
+        
+    return text
+
+if __name__ == '__main__':
+    sample_pdf= "data/sample_receipt.pdf"
+    extracted_text = extract_text_from_pdf(sample_pdf)
+    print(extracted_text)
